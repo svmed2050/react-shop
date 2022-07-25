a2dafab8d0855d39d5820a78df7e11b8143d393f
Pavel <pavelsergienko7@gmail.com>
2022-07-25 21:12:37 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ function Footer() {
           <div className="footer-copyright">
             <div className="container">
             © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+            <a 
+            className="grey-text text-lighten-4 right" 
+            href="https://github.com/svmed2050/react-shop" target="_blank" 
+            rel="noreferrer"
+            >Repo</a>
             </div>
           </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@
 function Header() {
     return <nav className="#43a047 green darken-1">
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Shop</a>
+      <a href="/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repo</a></li>
+        <li><a href="https://github.com/svmed2050/react-shop" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
     </div>
   </nav>
